ddd441bf81cf294302feb0a0737c7ca853d95a7f
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-02-23 12:59:55 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
 function App() {
-  return <h2>accordion project setup</h2>;
+  //[title, setTitle] = useState(SingleQuestion);
+
+  return (
+    <h2>
+      accordion project setup
+      <BeachAccessIcon />
+    </h2>
+  );
 }
 
 export default App;
