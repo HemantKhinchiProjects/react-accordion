195eec0182c3a611f348ca8ca304ada562f876b8
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-02-23 14:35:35 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import data from './data';
 import SingleQuestion from './Question';
 
 function App() {
-  //[title, setTitle] = useState(SingleQuestion);
+  const [title, setTitle] = useState(SingleQuestion);
 
-  return (
-    <h2>
-      accordion project setup
-
-    </h2>
-  );
+  return <h2>accordion project setup</h2>;
 }
 
 export default App;
