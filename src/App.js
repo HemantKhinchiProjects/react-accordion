8978a3d5e6164b81c6271200a1988310e117a9c5
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-02-23 13:04:18 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
-//import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
 function App() {
   //[title, setTitle] = useState(SingleQuestion);
 
   return (
     <h2>
-      accordion project setup //
-      {/* <BeachAccessIcon /> */}
+      accordion project setup
+      <BeachAccessIcon />
     </h2>
   );
 }
